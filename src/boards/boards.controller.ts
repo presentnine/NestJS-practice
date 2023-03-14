045dc4e43c99d17ae61d9d91b340b85de72a3cde
Hyunku Kim <presentnine@naver.com>
2023-03-14 17:47:05 +0900

    feat: add crud functions of board module

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { BoardsService } from "./boards.service";
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import {Board, BoardStatus} from "./board.model";
+import {CreateBoardDto} from "./dto/create-board.dto";
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService: BoardsService) {
+    constructor(private boardsService: BoardsService) {}
 
+    @Get('/')
+    getAllBoards(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoardById(@Param('id') id: string): void {
+        this.boardsService.deleteBoardById(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,
+    ): Board {
+        return this.boardsService.updateBoardStatusById(id, status);
     }
 }
